c07f4c91c5fd349b018759e4bbbf644def35c051
Thitipon <thitiphon.s@ku.th>
2021-02-08 18:45:44 +0700

    Update lab2_2.c

--- lab2_2.c
+++ lab2_2.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 int main()
 {
-    float time,m1=199,m2=299,a,b,c=200,d=400;
+    float time,m1=199,m2=299,a,b;
     char pro;
     scanf("%c",&pro);
     fflush(stdin);
     scanf("%f",&time);
     if(pro=='A')
     {
-        if(time<=c)
+        if(time<=200)
         {
             printf("%.2f",m1);
         }
         else
         {
-            a = (int)time - c;
+            a = (int)time - 200;
             a = a * 3;
-            printf("%.2f",m1+a+1);
+            printf("%.2f",m1+a);
         }
     }
     else if(pro=='B')
     {
-        if(time<=d)
+        if(time<=400)
         {
             printf("%.2f",m2);
         }
         else
         {
-            b = (int)time - d;
+            b = (int)time - 200;
             b = b * 2;
-            printf("%.2f",m2+b+1);
+            printf("%.2f",m2+b);
         }
     }
 }
