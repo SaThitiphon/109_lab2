dbbcf78af68d206320b880bebd65d42c95f7ccf7
Thitipon <thitiphon.s@ku.th>
2021-02-08 17:13:35 +0700

    lab2_3 ver1.1

---
+++ lab2_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s[4] = "";
+    int i,a[3];
+    for(i=0;i<3;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    fflush(stdin);
+    gets(s);
+    if(!strcmp(s,"ABC"))
+    {
+        
+    }
+}
